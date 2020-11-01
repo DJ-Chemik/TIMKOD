0cbace4a3220900be6447abe2ef5f141d6b9f0a1
dj-chemik <dj-chemik@wp.pl>
2020-11-01 16:31:19 +0100

    Add fie reader

--- src/Task1/Task1Main.tsx
+++ src/Task1/Task1Main.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+// eslint-disable-next-line
+import React, { useState } from 'react';
+
+
+const file = '../../task1/files/norm_hamlet.txt';
 
 const Task1Main = () => {
+    const [text, setText] = useState<any>('');
+
+    const showFile = async (files: FileList | null) => {
+        let file;
+        if (files) {
+            file = files[0];
+        }
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            const content = reader.result;
+            setText(content);
+        }
+        file && reader.readAsText(file);
+    }
+
 
     return(
         <div>
             Task 1
+            <div style={{margin: '2rem'}}>
+                <input type="file" onChange={(e) => showFile(e.target.files)} />
+                <div style={{margin: '2rem'}}>
+                    {text}
+                </div>
+            </div>
         </div>
     );
 }
